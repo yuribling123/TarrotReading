d85c63a30c1eb346068f68179a10721075a6a8f1
milktea <yuribling123@gmail.com>
2026-08-23 00:50:52 +0800

    v1

--- lib/tarot/index.ts
+++ lib/tarot/index.ts
@@ -1,4 +1,3 @@
-export { drawCards, tarotDeck } from "@/lib/tarot/deck";
 export { localizeTarotName } from "@/lib/tarot/localization";
 export { isDrawnCard } from "@/lib/tarot/validation";
 export type { DrawnCard, TarotCard } from "@/lib/types";

f40a47f8f6573e7fe26d90928e3ad94d52bb3c1c
milktea <yuribling123@gmail.com>
2026-08-28 23:30:13 +0800

    ui

--- app/components/question-form.tsx
+++ app/components/question-form.tsx
@@ -5,6 +5,7 @@ import { toast } from "@/components/ui/toast";
 import type { Language } from "@/lib/types";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
 
 const maxQuestionLength = 180;
 
@@ -53,7 +54,7 @@ export function QuestionForm({
     <form className="questionForm " onSubmit={submitQuestion}>
       {/* <label htmlFor="question"  >{label}</label> */}
       <div className="relative w-full">
-      <Input id="question" value={question} onBlur={() => window.scrollTo(0, 0)}  onChange={(event) => setQuestion(event.target.value)} placeholder={placeholder} autoComplete="off" lang={language} className=" h-17 text-[16px]! placeholder:text-[0.86rem] bg-white border-primary/70 text-[#4f463d] focus-visible:ring-0 focus-visible:border-primary caret-[#c98f9f]" />
+      <Input id="question" value={question} onBlur={() => window.scrollTo(0, 0)}  onChange={(event) => setQuestion(event.target.value)} placeholder={placeholder} autoComplete="off" lang={language} className="pr-21 h-17 text-[16px]! placeholder:text-[0.86rem] bg-white border-primary/70 text-[#4f463d] focus-visible:ring-0 focus-visible:border-primary caret-[#c98f9f]" />
      
         {/* 强制16字体：输入时才不会自动放大 */}
         {/* <input
@@ -67,7 +68,7 @@ export function QuestionForm({
         />
         <button type="submit">{submitLabel}</button>
        */}
-       <button type="submit" className="text-primary absolute right-4 top-1/2 -translate-y-1/2"> <p className="font-medium">{submitLabel}</p>  </button>
+       <Button type="submit" className="absolute right-3 top-1/2 -translate-y-1/2 h-10 px-5 rounded-full border border-[#d8bc78]/60 bg-[#e6cb7e]/20 text-[#9b722a] shadow-none transition-all duration-300 hover:bg-[#e6cb7e]/35 hover:text-[#7d5b20] hover:border-[#d8bc78] active:scale-[0.97]"> <p className="font-medium">{submitLabel}</p>  </Button>
         </div>
         
       

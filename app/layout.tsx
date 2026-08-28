980c8a09a9f6d07a741ac6003952c007085da6a7
milktea <yuribling123@gmail.com>
2026-08-28 15:32:10 +0800

    +optmize

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,10 @@ import { Navigation } from "@/app/components/navigation";
 import { ReadingSessionProvider } from "@/app/components/reading-session-provider";
 import { Toaster } from "@/components/ui/toast";
 import "./globals.css";
-import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/next"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export const metadata: Metadata = {
   title: "Moonlit Tarot",
@@ -20,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans")}>
       <body>
         <Toaster />
         <Analytics />

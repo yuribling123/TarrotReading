b686f0e6839cc098582cdebd6a625304627ffb63
milktea <yuribling123@gmail.com>
2026-08-28 20:58:17 +0800

    +ui

--- app/components/question-form.tsx
+++ app/components/question-form.tsx
@@ -4,6 +4,7 @@ import { type FormEvent, useState } from "react";
 import { toast } from "@/components/ui/toast";
 import type { Language } from "@/lib/types";
 import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input"
 
 const maxQuestionLength = 180;
 
@@ -49,12 +50,14 @@ export function QuestionForm({
   return (
     <div>
           
-    <form className="questionForm overflow-hidden " onSubmit={submitQuestion}>
+    <form className="questionForm  -translate-y-40 " onSubmit={submitQuestion}>
       <label htmlFor="question"  >{label}</label>
-      <div className="inputRow ">
+      <div className="relative w-full">
+      <Input id="question" value={question} onChange={(event) => setQuestion(event.target.value)} placeholder={placeholder} autoComplete="off" lang={language} className=" h-15 text-[16px]！ placeholder:text-[0.86rem] bg-white border-primary/70 text-primary  focus-visible:ring-0 focus-visible:border-primary" />
+     
         {/* 强制16字体：输入时才不会自动放大 */}
-        <input
-          className="text-[16px]! placeholder:text-[0.86rem]!"
+        {/* <input
+          className="text-[16px]! placeholder:text-[0.86rem]"
           id="question"
           value={question}
           onChange={(event) => setQuestion(event.target.value)}
@@ -63,8 +66,10 @@ export function QuestionForm({
           lang={language}
         />
         <button type="submit">{submitLabel}</button>
-      </div>
-
+       */}
+       <button type="submit" className="text-primary absolute right-4 top-1/2 -translate-y-1/2"> <p className="font-medium">{submitLabel}</p>  </button>
+        </div>
+        
       
     </form>
      

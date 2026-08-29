f1f3eba915465260763a0a06aa1c883429e8c726
milktea <yuribling123@gmail.com>
2026-08-30 00:08:46 +0800

    ui

--- app/components/moon-sign-daily.tsx
+++ app/components/moon-sign-daily.tsx
@@ -8,7 +8,7 @@ export function DailyZodiac() {
   <button className="group  mx-auto -translate-y-30 flex w-fit flex-col items-center gap-1.5 rounded-full border border-white/15 bg-[#292438]/50 px-3.5 py-3.5 shadow-[0_5px_20px_rgba(20,16,30,0.18)] backdrop-blur-md transition-all duration-300 hover:bg-[#332c45] active:scale-[0.97] active:bg-[#332c45]">
 
   {/* 第一行 */}
-  <div className="flex items-center gap-1.5 text-white/70">
+  <div className="flex items-center gap-1.5 text-[#F4F0E8]/65">
     <span className="text-[11px] tracking-[0.12em] ">
       月亮正落在
     </span>
@@ -19,11 +19,9 @@ export function DailyZodiac() {
   </div>
 
   {/* 第二行 */}
-  <div className="flex items-center text-[#F4F0E8]/80 pt-0.5">
-  <span className="text-[10px] tracking-[0.12em] ">
-    今夜提示：
-  </span>
-  <span className="text-[10px] tracking-[0.12em]">
+  <div className="flex items-center text-[#F4F0E8]/85 pt-0.5">
+  
+  <span className="text-[10px] tracking-[0.3em]">
    {moonSignMessage}
   </span>
   </div>

d9cbec017e4832795163b7e6525ac9e143173a10
milktea <yuribling123@gmail.com>
2026-08-29 23:05:38 +0800

    ui

--- app/components/feedback.tsx
+++ app/components/feedback.tsx
@@ -34,13 +34,13 @@ export function FeedBack() {
     }
 
     return (
-        <div className="mt-2 flex flex-col items-center ">
-            <p className="mt-2 text-[10px] text-[#595858]/80 self-center">听说留下共鸣的人，牌意更容易被宇宙收到 ✦</p>
-            <Button size="lg" onClick={handleOnClick} variant="secondary" disabled={feedback || isPending} className="mt-2 w-22 self-center">
+        <div className=" flex flex-col items-center ">
+            <p className="mt-4 text-[10px] text-[#595858]/80 self-center">听说留下共鸣的人，牌意更容易被宇宙收到 ✦</p>
+            <Button size="lg" onClick={handleOnClick} variant="secondary" disabled={feedback || isPending} className="mt-4 w-22 self-center">
                 {feedback ? <p>☾ 月光收下了</p> : <p>♡ 留下共鸣</p>}
             </Button>
             
-            <p className="mt-5  text-[9px] font-medium text-[#383140]/90 self-center">今夜已有 {count} 份共鸣</p>
+            <p className="mt-4  text-[9px] font-medium text-[#383140]/90 self-center">今夜已有 {count} 份共鸣</p>
 
         </div>
 

3685c562afafc37e1663bcb31f03abbf5e005127
milktea <yuribling123@gmail.com>
2026-08-31 23:50:55 +0800

    ui

--- app/components/moon-sign-daily.tsx
+++ app/components/moon-sign-daily.tsx
@@ -15,22 +15,22 @@ export function DailyZodiac() {
   
 
   return (
-    <button className="group  mx-auto -translate-y-40 flex w-fit flex-col items-center gap-1.5 rounded-[28px] border border-white/15 bg-[#292438]/50 px-5 py-3.5 shadow-[0_5px_20px_rgba(20,16,30,0.18)] backdrop-blur-[4px] transition-all duration-300 hover:bg-[#332c45]/40 active:scale-[0.97] active:bg-[#332c45]/40  ">
-      <div className="flex items-center text-[#F4F0E8]/60 pt-0.5 tracking-[0.10em]">
+    <button className="group  mx-auto -translate-y-40 flex w-fit flex-col items-center gap-1.5 rounded-[28px] border border-white/15 bg-[#292438]/50 px-5 py-3.5 shadow-[0_5px_20px_rgba(20,16,30,0.18)] backdrop-blur-[2px] transition-all duration-300 hover:bg-[#332c45]/40 active:scale-[0.97] active:bg-[#332c45]/40  ">
+      <div className="flex items-center text-[#F4F0E8]/70 pt-0.5 tracking-[0.10em]">
         <span className="text-[10px] ">
          {date} 
         </span>
       </div>
 
       {/* 第一行 */}
-      <div className="flex items-center gap-1.5 text-[#F4F0E8]/85">
+      <div className="flex items-center gap-1.5 text-[#F4F0E8]/90">
 
         <span className="text-[11px] tracking-[0.12em] ">    月亮行至{moonSign}</span>
 
       </div>
 
       {/* 第二行 */}
-      <div className="flex items-center text-[#F4F0E8]/60 pt-0.5 pb-0.5">
+      <div className="flex items-center text-[#F4F0E8]/70 pt-0.5 pb-0.5">
 
         <span className="text-[10px] tracking-[0.13em]">
           {moonSignMessage}

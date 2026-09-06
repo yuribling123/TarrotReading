515366e8d88b4cbe6bb0fdbbf6ab4a4d1175aa8b
milktea <yuribling123@gmail.com>
2026-09-06 23:06:46 +0800

    ui

--- app/components/card-fan.tsx
+++ app/components/card-fan.tsx
@@ -99,7 +99,7 @@ export function CardFan({
   return (
     <div
       ref={viewportRef}
-      className="cardFanViewport max-w-full touch-pan-x overflow-x-auto overflow-y-hidden overscroll-x-contain   lg:pt-30 md:pt-25 sm:pt-40 scrollbar-none  max-[520px]:w-full max-[520px]:mt-10"
+      className="cardFanViewport max-w-full touch-pan-x overflow-x-auto overflow-y-hidden overscroll-x-contain  scrollbar-none mt-40 max-[520px]:w-full max-[520px]:mt-5 max-[1024px]:mt-30 "
     >
       <div className={`cardFan flex w-max items-end ${spread ? "spreadOut" : "stacked"}`}>
         {deck.map((card, index) => (

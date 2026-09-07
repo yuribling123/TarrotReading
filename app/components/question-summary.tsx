7027de68fc92c276338a114a84c95012df715af8
milktea <yuribling123@gmail.com>
2026-09-07 11:51:12 +0800

    ui

--- app/components/question-summary.tsx
+++ app/components/question-summary.tsx
@@ -5,7 +5,7 @@ type QuestionSummaryProps = {
 export function QuestionSummary({ question }: QuestionSummaryProps) {
   return (
     <>
-      <div className="mt-20 lg:mt-35 flex justify-center">
+      <div className="mt-16 flex justify-center sm:mt-24 lg:mt-32">
         <span className="text-[9px]  font-medium drop-shadow-[0_1px_1px_rgba(60,55,50,0.28)]  tracking-[0.28em] text-[#9b722a]/60">
           今夜所问
         </span>

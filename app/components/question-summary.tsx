442df218856229f7b9d827f1590c59e32a242065
milktea <yuribling123@gmail.com>
2026-09-09 15:20:38 +0800

    ui

--- app/components/question-summary.tsx
+++ app/components/question-summary.tsx
@@ -45,7 +45,7 @@ export function QuestionSummary({ question }: QuestionSummaryProps) {
         </p>
 
       </div>
-      <div className="mt-1 h-px w-8 bg-[#b89552]/25" />
+      {/* <div className="mt-1 h-px w-8 bg-[#b89552]/25" /> */}
     </>
   );
 }

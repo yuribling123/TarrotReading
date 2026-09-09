ba8fc195dcd0dc1d23d8115149a8b76f5829e6d7
milktea <yuribling123@gmail.com>
2026-09-09 15:46:02 +0800

    ui

--- app/components/question-summary.tsx
+++ app/components/question-summary.tsx
@@ -6,7 +6,7 @@ export function QuestionSummary({ question }: QuestionSummaryProps) {
   return (
     <>
       <div className="mt-16 flex justify-center sm:mt-24 lg:mt-32">
-        <span className="text-[0.82rem]  font-medium drop-shadow-[0_1px_1px_rgba(60,55,50,0.28)]  tracking-[0.28em] text-[#70627D]">
+        <span className="text-[0.82rem]!  font-medium drop-shadow-[0_1px_1px_rgba(60,55,50,0.28)]  tracking-[0.28em] text-[#70627D]">
           今夜所问
         </span>
       </div>
@@ -21,8 +21,6 @@ export function QuestionSummary({ question }: QuestionSummaryProps) {
     overflow-y-hidden
     whitespace-nowrap
     text-center
-    text-[1rem]
-    font-normal
     leading-[1.7]
     tracking-[0.06em]
     text-[#3f352c]
@@ -35,7 +33,7 @@ export function QuestionSummary({ question }: QuestionSummaryProps) {
           className="
             pt-2
             text-center
-            text-[0.82rem]
+            text-[0.82rem]!
             font-medium
             tracking-[0.06em]
             text-[#342d3d]/85

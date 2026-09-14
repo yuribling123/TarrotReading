a8a8dc74fc5c1fd3a28038497a2fccbb229a1b53
milktea <yuribling123@gmail.com>
2026-09-14 22:10:31 +0800

    css

--- app/components/home/home-background.tsx
+++ app/components/home/home-background.tsx
@@ -48,7 +48,7 @@ export function HomeBackground() {
       ))}
 
 
-      <HomeLunarPath className="absolute bottom-[calc(env(safe-area-inset-bottom)+1.25rem)] left-1/2 -translate-x-1/2 md:bottom-6" />
+      {/* <HomeLunarPath className="absolute bottom-[calc(env(safe-area-inset-bottom)+1.25rem)] left-1/2 -translate-x-1/2 md:bottom-6" /> */}
     </div>
   );
 }

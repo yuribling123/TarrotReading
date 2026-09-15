97ad4e7d433ad0c1912f892c740752a7f1e305db
milktea <yuribling123@gmail.com>
2026-09-15 13:52:10 +0800

    css

--- app/components/home/home-background.tsx
+++ app/components/home/home-background.tsx
@@ -3,10 +3,10 @@ import { HomeLunarPath } from "./home-lunar-path";
 const moonDust = [
   { left: "28%", delay: "0s", duration: "5s", size: "3px", color: "#7f5b1f", type: "star" },
   { left: "32%", delay: "-2s", duration: "5s", size: "1px", color: "#c997d8", type: "star" },
-  { left: "40%", delay: "-1s", duration: "6s", size: "6px", color: "#c997d8", type: "star" },
-  { left: "48%", delay: "-4s", duration: "6s", size: "6px", color: "#7f5b1f", type: "star" },
+  { left: "36%", delay: "-1s", duration: "6s", size: "6px", color: "#c997d8", type: "star" },
+  { left: "42%", delay: "-4s", duration: "6s", size: "6px", color: "#7f5b1f", type: "star" },
   { left: "52%", delay: "-1s", duration: "7s", size: "4px", color: "#c997d8", type: "dot" },
-   { left: "62%", delay: "-1s", duration: "7s", size: "3px", color: "#7f5b1f", type: "dot" },
+   { left: "57%", delay: "-1s", duration: "7s", size: "3px", color: "#7f5b1f", type: "dot" },
   { left: "60%", delay: "-2s", duration: "5s", size: "3px", color: "#7f5b1f", type: "dot" },
   { left: "65%", delay: "-4s", duration: "5s", size: "2px", color: "#c997d8", type: "dot" },
 ];
